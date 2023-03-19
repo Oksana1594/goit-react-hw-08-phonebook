976f9da4c7067eba4ac7b2c573f172906e54f09c
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-03-19 19:11:45 +0000

    Deploying to gh-pages from @ Oksana1594/goit-react-hw-08-phonebook@71c122802ffd448592755c8e127cfb2e4344b942 🚀

---
+++ static/js/629.5760689c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[629],{629:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsx)("h2",{children:"Welcome to Phonebook"})}}}]);
+//# sourceMappingURL=629.5760689c.chunk.js.map
---
+++ static/js/83.369e8c86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[83],{83:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"NotFoundPage"})}}}]);
+//# sourceMappingURL=83.369e8c86.chunk.js.map
